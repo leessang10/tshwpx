bfb868204db60fbb39edd64403130c3bdf676f29
Sangmu Lee <sangmu.lee@codecrain.com>
2026-05-18 14:14:09 +0900

    chore: scaffold tshwpx package

---
+++ src/index.ts
@@ -0,0 +1,5 @@
+export { App, Hwp } from "./app";
+export { DocumentApi } from "./doc";
+export { LowLevelApi } from "./low/low-level-api";
+export { HwpAutomationError } from "./com/errors";
+export type { AppOptions, OpenOptions, SaveFormat } from "./app";
